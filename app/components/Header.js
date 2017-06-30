d1dd1ac199398e04d74d46b14ce4098f3605cb6b
root <root@balance-gaming-dev.localdomain>
2017-06-30 12:51:05 +0000

    mobile menu

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,10 +2,33 @@ var React = require('react');
 var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
 
-var Header = (props) =>{
+
+const Header = React.createClass({
+//var Header = (props) =>{
+
+getInitialState() {
+    return { mobileClass : "headerMenuSmall" };
+  },
+
+render: function(){
+
+
+  var activateMobileMenu =(active)=>{
+    if(this.state.mobileClass == "headerMenuSmall"){
+      this.setState({mobileClass : "headerMenuSmallActive"})
+    }else{
+      this.setState({mobileClass : "headerMenuSmall"})
+    }
+    if (active == "inactive"){
+      this.setState({mobileClass : "headerMenuSmall"})
+    }
+
+    console.log("changing mobile Class to active",this.state.mobileClass);
+  }
+
 
     var sliderOffset = -8; //Home
-    switch(props.activeTab.substring(1)){
+    switch(this.props.activeTab.substring(1)){
       case "Rosters":
         sliderOffset = 63;
         break;
@@ -84,36 +107,66 @@ var Header = (props) =>{
 
             </div>
 
-            <div className="headerMenuSmall hidden-md hidden-lg hidden-xl">
+            <div className={this.state.mobileClass == "headerMenuSmall" ? 'headerMenuSmall hidden-md hidden-lg hidden-xl' : 'headerMenuSmallActive hidden-md hidden-lg hidden-xl'}>
 
               <ul className="MenuULSmall col-xs-12 col-sm-8 col-sm-offset-2 noPadding">
 
-
-                <Link to={{ pathname:'/News', state: { scrollToTop: true } }}>
-                  <li>News</li>
+                <Link to={{ pathname:'/Home', state: { scrollToTop: true } }}>
+                  <img src={require('../imgs/Balance_Logo_B_grad.png')} style={{ height:72 , paddingTop:6}} onClick={ ()=>{ activateMobileMenu("inactive") } }/>
                 </Link>
 
-                <Link to={{ pathname:'/Rosters', state: { scrollToTop: true } }}>
-                  <li>Rosters</li>
-                </Link>
 
-                <Link to={{ pathname:'/Streams', state: { scrollToTop: true } }}>
-                  <li>Streams</li>
-                </Link>
-
-                <Link to={{ pathname:'/Media', state: { scrollToTop: true } }}>
-                  <li>Media</li>
-                </Link>
-
-                <Link to={{ pathname:'/Partners', state: { scrollToTop: true } }}>
-                  <li>Partners</li>
-                </Link>
-
-                <Link to={{ pathname:'/About', state: { scrollToTop: true } }}>
-                  <li>About</li>
-                </Link>
-
+                <div className="fa fa-bars" aria-hidden="true"
+                  style={{
+                    fontSize:36,
+                    cursor: 'pointer',
+                  }}
+                  onClick={ ()=>{ activateMobileMenu() } }
+                  >
+                </div>
               </ul>
+
+
+                {
+                  this.state.mobileClass == "headerMenuSmallActive"
+
+                  ?
+                  <ul className="MenuULSmallActive">
+
+                    <Link to={{ pathname:'/News', state: { scrollToTop: true } }} onClick={ ()=>{ activateMobileMenu("inactive") }} >
+                      <li>News</li>
+                    </Link>
+
+                    <Link to={{ pathname:'/Rosters', state: { scrollToTop: true } }} onClick={ ()=>{ activateMobileMenu("inactive") }} >
+                      <li>Rosters</li>
+                    </Link>
+
+                    <Link to={{ pathname:'/Streams', state: { scrollToTop: true } }} onClick={ ()=>{ activateMobileMenu("inactive") }} >
+                      <li>Streams</li>
+                    </Link>
+
+                    <Link to={{ pathname:'/Media', state: { scrollToTop: true } }} onClick={ ()=>{ activateMobileMenu("inactive") }} >
+                      <li>Media</li>
+                    </Link>
+
+                    <Link to={{ pathname:'/Partners', state: { scrollToTop: true } }} onClick={ ()=>{ activateMobileMenu("inactive") }} >
+                      <li>Partners</li>
+                    </Link>
+
+                    <Link to={{ pathname:'/About', state: { scrollToTop: true } }} onClick={ ()=>{ activateMobileMenu("inactive") }} >
+                      <li>About</li>
+                    </Link>
+
+
+                  </ul>
+
+                  :
+
+                  null
+
+                }
+
+
 
               <div className="headerSlider hidden-xs hidden-sm"
                 style={{
@@ -132,6 +185,7 @@ var Header = (props) =>{
 
       </div>
     )
-}
+  }
+})
 
 export default Header
